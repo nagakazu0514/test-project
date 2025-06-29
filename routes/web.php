e60941ce8279735ba6d6233b9b3055f620500404
nagaishikazuya <>
2025-06-29 10:27:14 +0900

    MVCモデルを実装しコミット

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
+Route::get('/test',[TestController::class,'test'])
+->name('test');
 
 Route::get('/', function () {
     return view('welcome');
